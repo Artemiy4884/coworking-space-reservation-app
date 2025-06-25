107781c1378e9d465b68c002e21aa746f7070f8f
Artemiy <artemiytev@gmail.com>
2025-06-25 16:48:35 +0200

    Added JPA and Hibernate to the project

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -2,42 +2,32 @@ package dao;
 
 import entities.User;
 
-import java.sql.*;
-import java.util.ArrayList;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
 import java.util.List;
 
-public class UserDAO {
+public class UserDAO extends BaseDAO{
 
-    public static List<User> getAllUsers() throws SQLException {
-        List<User> users = new ArrayList<>();
-        String sql = "SELECT * FROM users";
-
-        try (Connection connection = DBConnector.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resSet = statement.executeQuery(sql)) {
-
-            while (resSet.next()) {
-                users.add(new User(
-                        resSet.getString("username"),
-                        resSet.getString("password"),
-                        resSet.getString("role")
-                ));
-            }
+    public static List<User> getAllUsers() {
+        EntityManager entityManager = entityManager();
+        try {
+            TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u", User.class);
+            return query.getResultList();
+        } finally {
+            entityManager.close();
         }
-
-        return users;
     }
 
-    public static void addUser(User user) throws SQLException {
-        String sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
-
-        try (Connection connection = DBConnector.getConnection();
-             PreparedStatement prepStatement = connection.prepareStatement(sql)) {
-
-            prepStatement.setString(1, user.getUsername());
-            prepStatement.setString(2, user.getPassword());
-            prepStatement.setString(3, user.getRole());
-            prepStatement.executeUpdate();
+    public static void addUser(User user) {
+        EntityManager entityManager = entityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(user);
+            entityManager.getTransaction().commit();
+        } finally {
+            if (entityManager.getTransaction().isActive()) entityManager.getTransaction().rollback();
+            entityManager.close();
         }
     }
 }
